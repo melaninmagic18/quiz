5a784bd2cd77f43dcb6e71b5f807cbce8b138451
Rea Gibson <gibsonrea18@gmail.com>
2018-12-19 23:14:47 +0000

    day2

--- js/script.js
+++ js/script.js
@@ -11,4 +11,38 @@ $(document).ready(function() {
         var totalScore;
     });
 
-});
+
+    function oil(q1Result) {
+        if (q1Result==="castor oil") {
+        return 2; }
+         else if (q1Result==="coconut oil") {
+        return 3;}
+        else if (q1Result ==="tea tree oil"){
+        return 1;
+        }
+        return 0;
+        
+    }
+    function product(q2Result) {
+        if(q2Result==="shea moisture") {
+        return 3; }
+        else if (q2Result==="cantu") {
+        return 2; }
+        else if (q2Result==="carol's daughter") {
+        return 4;
+        }
+        return 0;
+    }
+        
+    function gel(q3Result) {
+        if (q3Result==="eco style") {
+        return 5; }
+        else if (q3Result==="gorilla snot") {
+        return 4; }
+        else if (q3Result==="wetline xtreme") {
+        return 3; }
+        else { return 0;}
+        
+        }
+        
+}
